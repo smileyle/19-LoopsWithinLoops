382ec983fd5de297fbc24e724dea7c679c750868
smileyle <smileyle@rose-hulman.edu>
2018-10-18 10:22:11 -0400

    All good!

--- src/m3_nested_loops_in_printing.py
+++ src/m3_nested_loops_in_printing.py
@@ -149,6 +149,13 @@ def run_test_decreasing_exclamation_marks():
 
 
 def decreasing_exclamation_marks(m, n):
+    string = ''
+    for k in range(m, n-1, -1):
+        for i in range(k):
+            string = string + '!'
+        print(string)
+        string = ''
+
     """
     Prints exclamation marks:  m on the first row,
     m-1 on the next row, m-2 on the next, etc, until n on the last row.
@@ -160,7 +167,7 @@ def decreasing_exclamation_marks(m, n):
     Precondition:  m and n are positive integers with m >= n.
     """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # Done: 4. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
@@ -191,6 +198,17 @@ def run_test_alternating_brackets():
 
 
 def alternating_brackets(m, n):
+
+    string = ''
+    for k in range(m - (n-1)):
+        for i in range(m-k):
+            if i % 2 == 0:
+                string = string + '['
+            else:
+                string = string + ']'
+        print(string)
+        string = ''
+
     """
     Prints alternating left/right square brackets:  m on the first row,
     m-1 on the next row, m-2 on the next, etc, until n on the last row.
@@ -202,7 +220,7 @@ def alternating_brackets(m, n):
     Precondition:  m and n are positive integers with m >= n.
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # Done: 5. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
@@ -233,6 +251,14 @@ def run_test_triangle_same_number_in_each_row():
 
 
 def triangle_same_number_in_each_row(r):
+    numba = 0
+    string = ''
+    for k in range(r):
+        numba += 1
+        for j in range(numba):
+            string += str(numba)
+        print(string)
+        string = ''
     """
     Prints a triangle of numbers, with r rows.
     The first row is 1, the 2nd is 22, the 3rd is 333, etc.
@@ -245,7 +271,7 @@ def triangle_same_number_in_each_row(r):
     Precondition:  r is a non-negative integer.
     """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # Done: 6. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
@@ -276,6 +302,13 @@ def run_test_triangle_all_numbers_in_each_row():
 
 
 def triangle_all_numbers_in_each_row(r):
+    string = ''
+    for k in range(r):
+        for i in range(k):
+            string = string + str(i+1)
+        print(str(string))
+        string = ''
+
     """
     Prints a triangle of numbers, with r rows.
     The first row is 1, the 2nd is 12, the 3rd is 123, etc.
@@ -288,7 +321,7 @@ def triangle_all_numbers_in_each_row(r):
     Precondition:  r is a non-negative integer.
     """
     # ------------------------------------------------------------------
-    # TODO: 7. Implement and test this function.
+    # Done: 7. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
